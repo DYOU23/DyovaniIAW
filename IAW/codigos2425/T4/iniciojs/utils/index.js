7f7284b494caf27bf33896e9e830bd07162efc17
DYOU23 <dyovani.integrae@gmail.com>
2025-02-06 03:58:46 +0100

    actualizacion

--- IAW/codigos2425/T4/iniciojs/utils/index.js
+++ IAW/codigos2425/T4/iniciojs/utils/index.js
@@ -1,3 +1,57 @@
 // esto es un ejemplo de fichero externo
-alert("esto se lanza solo");
+// alert("esto se lanza solo");
+
+console.log ("Esto es un msj desde un fichero")
+
+// variables: $nombre
+    // las que pueden cambiar su valor
+        // se usa let ej: let nombre="Borja"
+
+    // las que no pueden cambiar su dato
+        // se usa const ej: const DNI = "123123A"
+
+// metodos: ?
+
+
+
+
+let nombre = "Borja"; // eso es una variable string, o sea una palabra. Si quiero guardar una palabra defino con ""
+nombre = "BorjaM";
+
+let edad = 40; // eso es una variable numero. Si quiero guardar un number con int float
+edad = edad +1; // 41
+
+let experiencia = false // otra variable es boolean. Cuando solo se puede guardar valor true o false. 
+
+const DNI = "123123A";
+
+if (experiencia == true) {
+    console.log ("El usuario tiene experiencia");
+    // puedo por ejemplo, si tiene experiencia, pintar un div verde y además poner una animación
+} else {
+    console.log ("El usuario no tiene experiencia");
+    // pueto por ejemplo, si no tiene experiencia, pintar un div amarillo y sacar un cadro de dialogo avisandote
+}
+
+
+// busqueda por clase (ej: btn o btn-primary), etiqueta (ej: button) o identificador (ej: botonEnviar). Se suele utilizar id.
+
+let boton = document.querySelector("#botonEnviar");
+// si quiero buscar por id se pone # + id
+// si quiero buscar por clase se pone . + nombre de la clase
+// si quiero buscar por etiqueta se pone "nombre de la etiqueta"
+console.log(boton);
+
+// busqueda por varios al mismo tiempo. En este ejemplo por varios inputs
+let inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+// determino lo que pasa cuando pusan al boton
+boton.addEventListener("click", () => {
+    alert("Enhorabuena, reto completado")
+});
+
+
+
+
 
